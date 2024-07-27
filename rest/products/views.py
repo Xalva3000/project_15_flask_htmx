ffd0a39f41898c1af9d27ca64f1998e3a76ad93e
Xalva <maasania@gmail.com>
2024-07-27 12:29:58 +0400

    p-list htmx variant

--- rest/products/views.py
+++ rest/products/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, url_for
+from werkzeug.exceptions import BadRequest
+
 from .crud import products_storage
 
 
@@ -17,8 +19,14 @@ def get_products_list():
 def create_product():
     product_name = request.form.get("product_name", "").strip()
     product_price = request.form.get("product_price", "").strip()
-    if product_price.isdigit():
+    if product_price.isdigit() and product_name.isalpha():
         products_storage.add(product_name, int(product_price))
-
+    else:
+        raise BadRequest
+    # для стандартной формы
+    # return redirect(url_for("products_app.list"))
     products = products_storage.get_list()
-    return redirect(url_for("products_app.list"))
+    return render_template(
+        "products/components/products_list.html",
+        products=products
+    )
